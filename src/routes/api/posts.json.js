5ed8981a8cc578eed7b1836ca709b3c3bdb3e6c9
Kleidi Bujari <mail@kleidi.ca>
2022-07-19 21:16:29 -0400

    fix: build breaking changes

--- src/routes/api/posts.json.js
+++ src/routes/api/posts.json.js
@@ -1,4 +1,4 @@
-export const get = async () => {
+export const GET = async () => {
   const allPostFiles = import.meta.glob('../blog/*.md')
   const iterablePostFiles = Object.entries(allPostFiles)
 
