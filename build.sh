73ed2a79460bea26d85866c9ad6684d4a167fb6d
Kleidi Bujari <mail@kleidi.ca>
2022-07-21 10:55:27 -0400

    update: add png files to available photos

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 if which cwebp 2>/dev/null; then
   echo "Found cwebp, recompressing all source photos."
   rm static/gallery/*.webp
-  for file in static/gallery/*.jpg; do 
+  for file in static/gallery/*.{jpg,png}; do 
     echo compressing $file into webp 
     cwebp -quiet -q 20 $file -o $file.webp 
   done
