b2f45ed1e02fdc7ac5cf305e6e18f2f227d29665
Kleidi Bujari <mail@kleidi.ca>
2022-09-21 13:00:08 -0400

    update: image compression script improvements

--- build.sh
+++ build.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 if which cwebp 2>/dev/null; then
-  echo "Found cwebp, recompressing all source photos."
-  rm static/gallery/*.webp
+  echo "Found cwebp, attempting to compress all source photos."
+  # rm static/gallery/*.webp
   for file in static/gallery/*.jpg; do
-    echo compressing $file into webp
-    cwebp -quiet -q 20 $file -o $file.webp
+    if ! [ -f $file.webp ]; then
+      echo "compressing $file into webp"
+      cwebp -quiet -q 20 $file -o $file.webp
+    else
+      echo "$file.webp already exists"
+    fi
   done
 else
   echo "cwebp conversion tool not found, using existing photos."
