1030a4255b6c4b935b4a4a24292fca127495e97f
Kleidi Bujari <mail@kleidi.ca>
2022-11-01 10:15:58 -0400

    add font size change, fix footer applications, fix photos gap

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,8 +6,6 @@
   import '../app.css'
 
   export let data
-
-  const isRoot = data.currentRoute != '/'
   let menuActive = false
 </script>
 
@@ -16,13 +14,13 @@
 </svelte:head>
 
 {#key data.currentRoute}
-  {#if isRoot}
+  {#if data.currentRoute != '/'}
     <Header bind:sidebar={menuActive} />
     <Sidebar bind:menuActive />
   {/if}
   <main class="container z-0 my-24 font-body" in:fade={{ duration: 150, delay: 150 }} out:fade={{ duration: 150 }} class:blurify={menuActive}>
     <slot />
-    {#if isRoot}
+    {#if data.currentRoute != '/'}
       <Footer />
     {/if}
   </main>
--- src/routes/photos/+page.svelte
+++ src/routes/photos/+page.svelte
@@ -6,7 +6,6 @@
   const iterablePhotos = Object.entries(allPhotoFiles)
 
   const staticLength = '/static'.length
-  const gapSize = "4"
 </script>
 
 <svelte:head>
@@ -14,9 +13,9 @@
 </svelte:head>
 
 <Title text="Gallery" isBig={true} />
-<div class="columns-1 md:columns-2 xl:columns-3 gap-{gapSize} mt-{gapSize}">
+<div class="columns-1 md:columns-2 xl:columns-3 gap-4 mt-4">
   {#each iterablePhotos as photo}
     <ImageLoader src={photo[0].substring(staticLength)} alt="Image taken by myself" />
-    <div class="mb-{gapSize}" />
+    <div class="mb-4" />
   {/each}
 </div>
