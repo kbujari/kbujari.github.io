074c3b77698c655d11dffddac099142330535945
Kleidi Bujari <mail@kleidi.ca>
2023-02-12 17:30:07 -0500

    default csr/ssr

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,7 +1,7 @@
 import { error } from '@sveltejs/kit';
 
 export const prerender = true;
-export const csr = true;
+// export const csr = true;
 // export const ssr = true;
 
 export const load = async ({ url }) => {
