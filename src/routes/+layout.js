5513055efc2f22365efd2218bf6bf1bfa5e684ab
Kleidi Bujari <mail@kleidi.ca>
2023-02-12 17:02:41 -0500

    fix landing page not working

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -2,7 +2,7 @@ import { error } from '@sveltejs/kit';
 
 export const prerender = true;
 export const csr = true;
-//export const ssr = false
+// export const ssr = true;
 
 export const load = async ({ url }) => {
   try {
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,8 @@
 <script>
   import IconLinks from '$lib/components/IconLinks.svelte';
-  import { onMount } from 'svelte';
   import { ArrowRightCircleIcon } from 'lucide-svelte';
   import { fade } from 'svelte/transition';
 
-  let t = false;
-  onMount(() => (t = true));
 </script>
 
 <svelte:head>
@@ -13,16 +10,14 @@
 </svelte:head>
 
 <div class="bg-cover h-screen absolute inset-0 flex items-center justify-center backdrop-blur-2xl" style="background-image: url(/stars.jpg.webp);">
-  {#if t}
-    <div class="flex flex-col gap-6 justify-center items-center" in:fade={{ duration: 800 }}>
-      <p class="text-5xl text-slate-300 font-title font-bold pointer-events-none">Kleidi Bujari</p>
-      <IconLinks isSmall={false} />
-      <a href="/info" class="route">
-        <div class="flex items-center gap-4">
-          <p>Enter Site</p>
-          <ArrowRightCircleIcon strokeWidth="2" size="48" />
-        </div>
-      </a>
-    </div>
-  {/if}
+  <div class="flex flex-col gap-6 justify-center items-center" in:fade={{ duration: 800, delay: 650 }}>
+    <p class="text-5xl text-slate-300 font-title font-bold pointer-events-none">Kleidi Bujari</p>
+    <IconLinks isSmall={false} />
+    <a href="/info" class="route">
+      <div class="flex items-center gap-4">
+        <p>Enter Site</p>
+        <ArrowRightCircleIcon strokeWidth="2" size="48" />
+      </div>
+    </a>
+  </div>
 </div>
