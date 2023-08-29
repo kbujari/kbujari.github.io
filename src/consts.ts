3a1646164231fc261f7fa0f426a76e4cab14a1d1
Kleidi Bujari <mail@kleidi.ca>
2023-08-29 00:51:21 -0400

    clean site, add pgp key, clean footer, rename rss to feed

--- src/consts.ts
+++ src/consts.ts
@@ -1,15 +1,10 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
 export const SITE = {
   author: "Kleidi Bujari",
   description: "Minimal blog, portfolio and personal website.",
   title: "Kleidi Bujari",
 };
 
-// prettier-ignore
 export const LINKS = [
-  { href: 'mailto:mail@kleidi.ca', title: 'E-Mail', icon: 'mail' },
-  { href: 'https://github.com/kbujari', title: 'GitHub', icon: 'github' },
-  { href: 'https://www.linkedin.com/in/kleidib', title: 'LinkedIn', icon: 'linkedin' }
+  { href: 'https://github.com/kbujari', title: 'GitHub', icon: 'lucide:github' },
+  { href: 'https://www.linkedin.com/in/kleidib', title: 'LinkedIn', icon: 'lucide:linkedin' },
 ];
