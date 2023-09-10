74cb31ab5cdffaec12098866dd5382b01d24c4fe
Kleidi Bujari <mail@kleidi.ca>
2023-09-09 20:34:14 -0400

    revert back to original ports

--- astro.config.ts
+++ astro.config.ts
@@ -10,9 +10,14 @@ import robotsTxt from "astro-robots-txt";
 
 import remarkToc from "remark-toc";
 
+const port = 3000;
+
 const config = {
   site: "https://kleidi.ca",
-  server: { port: 3000 },
+  vite: {
+    server: { port },
+    preview: { port }
+  },
   markdown: {
     shikiConfig: {
       theme: "vitesse-dark",
