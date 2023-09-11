612416111ef42c3d29f30fd5e21e7dbe3736ce1d
Kleidi Bujari <mail@kleidi.ca>
2023-09-10 20:18:08 -0400

    fix undefined title

--- astro.config.ts
+++ astro.config.ts
@@ -10,14 +10,9 @@ import robotsTxt from "astro-robots-txt";
 
 import remarkToc from "remark-toc";
 
-const port = 3000;
 
 const config = {
   site: "https://kleidi.ca",
-  vite: {
-    server: { port },
-    preview: { port }
-  },
   markdown: {
     shikiConfig: {
       theme: "vitesse-dark",
