b30a25444eaa11c69a6d441979e69b2a2e8f280c
Kleidi Bujari <mail@kleidi.ca>
2023-09-24 20:06:26 -0400

    change to uppercase endpoint

--- src/pages/feed.xml.ts
+++ src/pages/feed.xml.ts
@@ -2,7 +2,7 @@ import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
 import { SITE } from "@consts";
 
-export const get = async () => {
+export const GET = async () => {
   const posts = await getCollection("posts");
   return rss({
     title: "RSS Feed | Kleidi Bujari",
