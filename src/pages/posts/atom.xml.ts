54cf2c237863d1a1c3a7e3feddc0bd7d7649424c
Kleidi Bujari <mail@4kb.net>
2024-06-23 14:38:46 -0400

    fix: filter out drafts in prod

--- src/pages/posts/atom.xml.ts
+++ src/pages/posts/atom.xml.ts
@@ -3,16 +3,18 @@ import { getCollection } from "astro:content";
 import { SITE } from "@consts";
 
 export const GET = async () => {
-  const posts = await getCollection("posts");
+  const posts = await getCollection("posts", ({ data }) => {
+    return import.meta.env.DEV || !data.title.startsWith("[draft]");
+  });
 
   return rss({
     title: "RSS Feed | Kleidi Bujari",
     description: SITE.description,
     site: import.meta.env.SITE,
-    items: posts.map((post) => ({
-      link: `/posts/${post.slug}/`,
-      pubDate: post.data.date,
-      ...post.data,
+    items: posts.map(({ slug, data }) => ({
+      link: `/posts/${slug}/`,
+      pubDate: data.date,
+      ...data,
     })),
   });
 };
